c49e8363e154c5398623841228ea66f71551b6cc
adrienltl <adrien.latil@insa-rouen.fr>
2017-03-17 10:29:43 +0100

    salut je viens de modifier le fichier, a vous de finir le taf

---
+++ prgm1/src/prgm1.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : prgm1.cpp
+// Author      : adrien
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "premiere modif";
+	return 0;
+}
